2ad29915e2103f03116b6bffeb63c1f215cfeaa8
Win_victor <win-victor@mail.ru>
2022-02-20 20:27:28 +0300

    created superuser, db upload script

---
+++ users/management/commands/fill.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from users.models import ProUser
+from django.contrib.auth.models import User
+import json, os
+
+JSON_PATH = 'users/json'
+
+
+def load_from_json(file_name):
+    with open(f'{settings.BASE_DIR}/json/{file_name}.json', 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        users = load_from_json('users')
+        ProUser.objects.all().delete()
+        for user in users:
+            ProUser.objects.create(**user)
+
+
+super_user = ProUser.objects.create_superuser(username='Джимми', first_name='Джеймс', last_name='Кэмерон', birthday='1954-08-16', email='django@geekshop.local',
+                                              password='geekbrains')
--- users/models.py
+++ users/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 
 class ProUser(AbstractUser):
-    username = models.CharField(max_length=64, unique=True)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    birthday = models.DateField(null=True)
-    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=64, unique=True, verbose_name='Ник')
+    first_name = models.CharField(max_length=64, verbose_name='Имя')
+    last_name = models.CharField(max_length=64, verbose_name='Фамилия')
+    birthday = models.DateField(null=True, verbose_name='Дата рождения')
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+
+    def __str__(self):
+        return self.username, self.id

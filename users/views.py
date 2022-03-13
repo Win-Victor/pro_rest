6491a89de66309e2a7a0ee6823b9dd4216e00fba
Win_victor <win-victor@mail.ru>
2022-03-13 17:18:02 +0300

    updated view for user model

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import GenericViewSet
 from users.serializers import ProUserModelSerializer
 from users.models import ProUser
 
 
-class ProUserViewSet(ModelViewSet):
+class ProUserViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     serializer_class = ProUserModelSerializer
     queryset = ProUser.objects.all()

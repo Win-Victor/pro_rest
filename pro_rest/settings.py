9fac7abf6711b7f453c3fa6df25c752370411ede
Win_victor <win-victor@mail.ru>
2022-03-20 15:54:15 +0300

    les_5

--- pro_rest/settings.py
+++ pro_rest/settings.py
@@ -115,7 +115,8 @@ REST_FRAMEWORK = {
     ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100,
-    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
 }
 
 if DEBUG:

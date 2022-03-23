3443a0e5fbc28fd2d94c758c5542e5ec7bde6a45
Win_victor <win-victor@mail.ru>
2022-03-23 22:25:13 +0300

    fixed admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# from users.models import ProUser
-#
-# admin.site.register(ProUser)
+from users.models import ProUser
+
+
+class DRFUserAdmin(UserAdmin):
+    model = ProUser
+    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff']
+
+
+admin.site.register(ProUser, DRFUserAdmin)
